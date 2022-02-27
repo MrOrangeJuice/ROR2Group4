2b8a15958841b5d9cd21e5398c75e5cd33eb65ec
Karl Sun <ks3678@ad.rit.edu>
2022-02-26 23:59:23 -0500

    transferred npc chat code and set it up in test scene

---
+++ ROR2Test/Assets/Assembly/Scripts/SendPlayerMessage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using UnityEngine;
+using BepInEx;
+using RoR2;
+using RoR2.ConVar;
+using RoR2.Networking;
+using UnityEngine.Networking;
+
+namespace ROR2RiskyBiscuits
+{
+    public class SendPlayerMessage : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            for (int i = 0; i < CharacterMaster.readOnlyInstancesList.Count; i++)
+            {
+                //CharacterMaster.readOnlyInstancesList[i] is the player.
+                if (other == CharacterMaster.readOnlyInstancesList[i].gameObject.GetComponent<Collider>())
+                {
+                    Chat.SendBroadcastChat(new Chat.SimpleChatMessage { baseToken = "<color=#e5eefc>{0}</color>", paramTokens = new[] { "TESTING TESTING" } });
+                }
+            }
+        }
+    }
+}
+
